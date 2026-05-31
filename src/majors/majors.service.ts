4b644f0d1093e5d5c8fac3775094a0bfb5f75b57
Thy Pharoth <phathyroth@gmail.com>
2026-05-31 20:21:45 +0700

    fix: add image_url for findall mojorsService

--- src/majors/majors.service.ts
+++ src/majors/majors.service.ts
@@ -9,7 +9,7 @@ export class MajorsService {
     const { data, error } = await this.supabaseService
       .getClient()
       .from('majors')
-      .select('id, name, acronym')
+      .select('id, name, acronym, image_url')
       .order('name');
 
     if (error) throw new InternalServerErrorException('Failed to fetch majors');
